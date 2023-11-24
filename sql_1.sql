266db969a14fc9cc76a27000b78f10405314f341
prabanch <prabanch@gmail.com>
2023-11-24 09:06:08 +0530

    Create  sql_1.sql
    1st

---
+++ sql_1.sql
@@ -0,0 +1,9 @@
+24. Find out the year between today and ’18-dec-18′.
+SELECT DATEDIFF(YEAR, '2018-12-18', CURRENT_DATE()) AS year_difference;
+
+25. Print next day.
+SELECT CURRENT_DATE+1;
+select next_day('2023-09-14','thursday') from dual;
+
+26. Print yesterday.
+SELECT CURRENT_DATE-1;
